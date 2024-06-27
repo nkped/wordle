9897c7f32970db92a49676fd2f380e98fc5f1896
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2024-06-27 18:08:29 +0200

    add more wordle logic

--- main.js
+++ main.js
@@ -5,36 +5,47 @@ const guessForm = document.querySelector("#gameForm")
 const userGuess = document.querySelector("#userGuess")
 
 
-guessForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    let guess = userGuess.value
-
-    console.log('guess.length is: ', guess.length)
-    
-    if(guess.length !== 6) {
-        alert('Sorry, guess must be 6 characters')
-        return
-    } else {
-        userGuess.value = ''
-    }
-
-    //console.log('ya guessed: ', guess)
-})
-
-
-
-
-
 
 
 //LOADS (INIT) WORDOFTHEDAY
 window.addEventListener('load', () => {
-    console.log('wellcome to Wordle-ish!!')
-    /* let wordOfTheDay = generateWord()
-    console.log(wordOfTheDay) */
+    
+    let wordString = generateWord()
+    console.log(wordString)
 
     ///GUESS LOGIC
-    //console.log("guess:", guess)
+    guessForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        let guessString = userGuess.value
+        
+        if(guessString.length !== 5) {
+            alert('Sorry, guess must be 5 characters')
+            return
+
+        } else if (guessString == wordString) {
+            userGuess.value = ''
+            console.log('correct guess!!')
+            return
+            
+        } else {
+            let guessArr = guessString.split('')
+            let wordArr = wordString.split('')
+
+
+            
+
+           /*  for (let i of guessArr) {
+                for (let j of wordArr ) {
+                    if (i == j) {
+                        console.log('match')
+                    } else {
+                        console.log('no match')
+                    }
+                }
+            } */
+        }
+    
+    })
 
 
 })
