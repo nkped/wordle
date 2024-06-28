bbae8e899fcad30f0f3f240f0bdec22b0db29b07
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2024-06-28 22:06:26 +0200

    just need to reverse arr

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ const guessForm = document.querySelector("#gameForm")
 const userGuess = document.querySelector("#userGuess")
 
 
-
-
 //LOADS (INIT) WORDOFTHEDAY
 window.addEventListener('load', () => {
     
@@ -30,24 +28,26 @@ window.addEventListener('load', () => {
         } else {
             let guessArr = guessString.split('')
             let wordArr = wordString.split('')
-
-
+            let results = []
+            let search = []
             
-
-           /*  for (let i of guessArr) {
-                for (let j of wordArr ) {
-                    if (i == j) {
-                        console.log('match')
-                    } else {
-                        console.log('no match')
-                    }
-                }
-            } */
-        }
-    
+            for (let i = 0; i < guessArr.length; i++) {
+            
+                if (guessArr[i] == wordArr[i]) {
+                    search.push(-1)
+                    results.push(2)
+                    } else if (guessArr.includes(wordArr[i])) {
+                    search.push(-1)
+                    results.push(1)      
+                    } else if (guessArr[i] != wordArr[i]) {
+                    search.push(-1)
+                    results.push(-1)
+                    }                               
+            }            
+            console.log('search: ', search)
+            console.log('result: ' ,results)
+        }    
     })
-
-
 })
 
 
